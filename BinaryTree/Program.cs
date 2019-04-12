4c544b27aecf3889135f57d29546c530d85064a7
Pavel_Silukov <434sp89@gmail.com>
2019-04-12 16:01:08 +0300

    Last Version

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -10,6 +10,25 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
+            BinaryTree<int> instance = new BinaryTree<int>();
+            instance.Add(8);
+            instance.Add(5);
+            instance.Add(13);
+            foreach (var item in instance)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(instance.Contains(1));
+            Console.WriteLine(instance.Contains(5));
+            Console.WriteLine();
+            instance.Remove(2);
+            instance.Remove(8);
+            foreach (var item in instance)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadKey();
         }
     }
 }
